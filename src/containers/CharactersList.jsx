c71e6a11f4c570331cba881cafb8d897e8786597
Laís Lima <lais@cosmobots.io>
2019-11-16 19:52:45 -0300

    :rocket: sorting results

--- src/containers/CharactersList.jsx
+++ src/containers/CharactersList.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import {
   ListGroup, ListGroupItem, ListGroupItemText,
-  Media, Spinner,
+  Media, Spinner, Button,
+  Card, Row, Col,
 } from 'reactstrap';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
-import { getCharacters } from '../actions/charactersAction';
+import { getCharacters, filterCharacters } from '../actions/charactersAction';
 
 const Label = styled.span`
   font-weight:600;
@@ -21,6 +22,39 @@ const SpinnerContainer = styled.div`
   width:100%;
 `;
 
+const ListHeader = styled.div`
+  padding:40px 20px 10px 20px;
+  font-size:24px;
+`;
+
+const ButtonsContainer = styled.div`
+  text-align:right;
+`;
+
+const SortIcon = styled.img`
+  height:20px;
+`;
+
+const compareCresc = ( a, b ) => {
+  if ( a.name < b.name ){
+    return -1;
+  }
+  if ( a.name > b.name ){
+    return 1;
+  }
+  return 0;
+}
+
+const compareDecresc = ( a, b ) => {
+  if ( a.name > b.name ){
+    return -1;
+  }
+  if ( a.name < b.name ){
+    return 1;
+  }
+  return 0;
+}
+
 const CharactersList = () => {
   const dispatch = useDispatch();
   const { filteredCharacters, loading, characters } = useSelector((state) => state.characters);
@@ -29,50 +63,69 @@ const CharactersList = () => {
     dispatch(getCharacters());
   }, []);
 
-  console.log("CAHRLIST", filteredCharacters);
-
+  const sortCresc = (list, order) => {
+    const orderedList = order === 'cresc' ? list.sort(compareCresc) : list.sort(compareDecresc);
+    dispatch(filterCharacters(orderedList));
+  };
 
   return (
-    <ListGroup>
-      {loading && (
-        <SpinnerContainer>
-          <Spinner type="grow" color="success" />
-        </SpinnerContainer>
-      )}
-      {filteredCharacters.length > 0 && filteredCharacters.map((character) => (
-        <ListGroupItem key={character.id}>
-          <Media>
-            <Media left top>
-              <CharacterImage object src={character.image} alt={`${character.name}'s photo`} />
-            </Media>
-            <Media body>
-              <Media heading>
-                { character.name }
+    <>
+      <ListHeader>
+        <Row>
+          <Col sm={6}>Resultados</Col>
+          <Col sm={6}>
+            <ButtonsContainer>
+              <Button outline color="success" style={{ marginRight: 10 }} onClick={() => sortCresc(filteredCharacters, 'cresc')}>
+                <SortIcon src="https://image.flaticon.com/icons/svg/109/109583.svg"  />
+              </Button>
+              <Button outline color="success" onClick={() => sortCresc(filteredCharacters, 'desc')}>
+                <SortIcon src="https://image.flaticon.com/icons/svg/109/109611.svg" />
+              </Button>
+            </ButtonsContainer>
+          </Col>
+        </Row>
+      </ListHeader>
+      <ListGroup>
+        {loading && (
+          <SpinnerContainer>
+            <Spinner type="grow" color="success" />
+          </SpinnerContainer>
+        )}
+        {filteredCharacters.length > 0 && filteredCharacters.map((character) => (
+          <ListGroupItem key={character.id}>
+            <Media>
+              <Media left top>
+                <CharacterImage object src={character.image} alt={`${character.name}'s photo`} />
               </Media>
-              <ListGroupItemText>
-                <div>
-                  <Label>Specie:</Label>
-                  { character.species }
-                </div>
-                <div>
-                  <Label>Gender:</Label>
-                  { character.gender }
-                </div>
-                <div>
-                  <Label>Status:</Label>
-                  { character.status }
-                </div>
-              </ListGroupItemText>
+              <Media body>
+                <Media heading>
+                  { character.name }
+                </Media>
+                <ListGroupItemText>
+                  <div>
+                    <Label>Specie:</Label>
+                    { character.species }
+                  </div>
+                  <div>
+                    <Label>Gender:</Label>
+                    { character.gender }
+                  </div>
+                  <div>
+                    <Label>Status:</Label>
+                    { character.status }
+                  </div>
+                </ListGroupItemText>
+              </Media>
             </Media>
-          </Media>
-        </ListGroupItem>
-      ))}
-      {characters.length === 0 && (
-        <ListGroupItem>
-          <h1>There is not...</h1>
-        </ListGroupItem>
-      )}
-    </ListGroup>
+          </ListGroupItem>
+        ))}
+        {characters.length === 0 && (
+          <ListGroupItem>
+            <h1>There is not...</h1>
+          </ListGroupItem>
+        )}
+      </ListGroup>
+    </>
   );
 };
 
