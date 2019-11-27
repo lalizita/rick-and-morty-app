cdcb447130fbaa7e32c845c6948105a1818ba6f9
Laís Lima <lais@cosmobots.io>
2019-11-27 13:44:39 -0300

    Updates

---
+++ precache-manifest.cb8a13dfced43477b7fcbc1be1e2a848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3744d413e83191f0d2935be16df8ff7a",
+    "url": "/rick-and-morty-app/index.html"
+  },
+  {
+    "revision": "52b9494622b702f3920d",
+    "url": "/rick-and-morty-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "52b9494622b702f3920d",
+    "url": "/rick-and-morty-app/static/js/2.d71fa418.chunk.js"
+  },
+  {
+    "revision": "310b9432f57c3c984c92",
+    "url": "/rick-and-morty-app/static/js/main.a2fe15ce.chunk.js"
+  },
+  {
+    "revision": "ec3a1f4c776e7fa4d0d7",
+    "url": "/rick-and-morty-app/static/js/runtime-main.21f83d2f.js"
+  },
+  {
+    "revision": "f8f3c3e177614d714df81f9294ee11ff",
+    "url": "/rick-and-morty-app/static/media/rickAndMorty.f8f3c3e1.png"
+  }
+]);
